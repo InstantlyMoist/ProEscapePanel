ebf74a8b5b2f54a0f7fb62ee68a634c7132577dc
Kyllian Warmerdam <kyllian007@gmail.com>
2021-11-22 11:53:14 +0100

    Changes in header

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ Route::add('/',function(){
     include ("./views/index.php");
 });
 
+Route::add('/room/add',function(){
+    include ("./views/room/add/index.php");
+});
+
 // Simple test route that simulates static html file
 Route::add('/room/([0-9]*)',function($var1){
     include ("./views/room/index.php");
