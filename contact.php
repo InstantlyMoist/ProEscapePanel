ca8dd88b648967203803ee79b70f57f5f04bf072
TimMuffin9 <91592837+TimMuffin9@users.noreply.github.com>
2021-12-20 11:17:49 +0100

    Made a contact page

---
+++ contact.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+
+    <link rel="stylesheet" href="../../material.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+</head>
+<body>
+    <div class="demo-layout-waterfall mdl-layout mdl-js-layout">
+    <?php include_once('../../header.php') ?>
+        <main class="mdl-layout__content">
+            <div class="page-content">
+                    <h1>Contact</h1>
+                    Deze opdracht is uitgevoerd in naam van Fontys Hogescholen. <br><br>
+                    <div class="flex w50">
+                        <div>
+                            Kim Veroude <br>
+                            Kyllian Warmerdam <br>
+                            Andreas Derksen <br>
+                            Melad Rahmani <br>
+                            Tim den Hartigh <br>
+                            <br>
+                            Professor Goossenslaan 1, gebouw P8 <br>
+                            5022 DM Tilburg
+                        </div>
+                        <div>
+                            k.veroude@student.fontys.nl<br>
+                            k.warmerdam@student.fontys.nl<br>
+                            a.derksen@student.fontys.nl<br>
+                            m.rahmani@student.fontys.nl<br>
+                            t.denhartigh@student.fontys.nl
+                        </div>
+                    </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
