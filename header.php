f2dda219f0ccfa6f490508ba155362fc86280cf6
Andreas <89839661+Andreasd21@users.noreply.github.com>
2022-01-14 14:47:09 +0100

    removed empty href

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
         <a href="https://localhost"><span class="mdl-layout-title"><img src="../../img/logo-pro-escape-dark-mode.png" alt="" height="40px"></span></a>
         <div class="mdl-layout-spacer"></div>
         <a href="https://localhost"><button type="button" class="mdl-button">Dashboard</button></a>
-        <a href="https://localhost/views/settings/account.php"><button type="button" class="mdl-button">Account</button></a>
         <a href="https://localhost/views/settings/contact.php"><button type="button" class="mdl-button">Contact</button></a>
 
         <div class="dropdown">
